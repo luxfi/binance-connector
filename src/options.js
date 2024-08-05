9aa2e7b32c538b8fed9ee7458e4a1192c366a99d
TopTech126 <carlie19782@gmail.com>
2024-08-05 18:34:00 +1000

    init

---
+++ src/options.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const APIBase = require('./APIBase')
+const modules = require('./futures-modules')
+const { flowRight } = require('./helpers/utils')
+
+const OptionModules = superclass => class extends superclass {
+  openOptionsPositions () {
+    return this.signRequest(
+      'GET',
+      '/eapi/v1/position',
+    )
+  }
+
+  getOptionsAccount () {
+    return this.signRequest(
+      'GET',
+      '/eapi/v1/account',
+    )
+  }
+}
+
+class Options extends flowRight(...Object.values({ OptionModules }))(APIBase) {
+  constructor (apiKey = '', apiSecret = '', options = {}) {
+    options.baseURL = options.baseURL || 'https://eapi.binance.com'
+    super({
+      apiKey,
+      apiSecret,
+      ...options
+    })
+  }
+}
+
+module.exports = Options;
